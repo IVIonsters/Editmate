cdf937f48830d2c20de3ce7fdf62a7d614a0cdf0
Zachary Polof <ZacharyPolof@gmail.com>
2024-07-18 20:35:48 -0500

    Update database.js
    added the getDb function to connect to the jate database
    
    retrieves data from jate store as read only
    
    logs response

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,18 +12,49 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+
 export const putDb = async (content) => {
   console.log(' putDb to be implemented');
+
+  //Create a new connection to the database
   const db = await openDB('jate', 1);
+
+  //Create a new transaction to write to the database
   const tx = db.transaction('jate', 'readwrite');
+
+  //Open the database object store
   const store = tx.objectStore('jate');
+
+  //Put the data in the object store
   const request = store.put({ id: 1, jate: content });
+
+  //Wait for the data to be saved
   const result = await request;
+
+  //Return the result
   console.log('Saved', result);
 }
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log(' getDb to be implemented');
+  //Create a new connection to the database
+  const db = await openDB('jate', 1);
+
+  //Create a new transaction to read from the database
+  const tx = db.transaction('jate', 'readonly');
+
+  //Open the database object store
+  const store = tx.objectStore('jate');
+
+  //Get the data from the object store
+  const request = store.get(1);
+
+  //Wait for the data to be retrieved
+  const result = await request;
+
+  //Return the data
+  console.log('Loaded', result);
+  return result;
+}
 
 initdb();
